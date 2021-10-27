505876fd398956d7ac164f149d6e4bb864c1c9e0
guzmandp <guzmandp@gmail.com>
2021-10-27 18:26:52 -0300

    update portfolio

---
+++ script.js
@@ -0,0 +1,73 @@
+var nome = document.getElementById("nome");
+var email = document.getElementById("email");
+var assunto = document.getElementById("assunto");
+var error = document.getElementById("error");
+error.style.color = "red";
+
+function enviarFormulario() {
+  console.log("Formulario Enviado");
+  var expresion = /\w+@\w+\.+[a-z]/;
+  var msgErro = [];
+
+  if (nome.value === null || nome.value === "") {
+    document.getElementById("nome").focus();
+    document.getElementById("nome").focus();
+    msgErro.push("⚠️ Digite um nome");
+  }
+
+  if (email.value === null || email.value === "") {
+    document.getElementById("email").focus();
+    msgErro.push("⚠️ Digite um email");
+  }
+
+  if (!expresion.test(email.value)) {
+    document.getElementById("email").focus();
+    msgErro.push("⚠️ Digite um email valido");
+  }
+
+  if (assunto.value === null || assunto.value === "") {
+    document.getElementById("assunto").focus();
+    msgErro.push("⚠️ Digite o asunto");
+  } else {
+    alert("Formulario enviado!!");
+    document.getElementById("form").reset();
+  }
+
+  error.innerHTML = msgErro.join(" ,          ");
+
+  return false;
+}
+
+////////////////////////////////////REPOSITORIO///////////////////////////////////////////////////
+const cardSection = document.querySelector("card__section");
+
+function repositoriosGitHub() {
+  fetch("https://api.github.com/users/guzmandp/repos")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      return cardSection;
+    });
+}
+
+function userGitHub() {
+  cardSection.innerHTML = `
+            <section class="box__section">
+            <div>
+                <img 
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGL44UakF8E7HxJ4uLZQFtflHc5vp8Tw4lZw&usqp=CAU" 
+                    alt="Fusce ultrices"
+                    class="img__cardBox"
+                    >
+            </div>
+            <div>
+                <h2 class="title__cardBox">${data.name}</h2>
+                <p class="text__cardBox">${data.clone_url}</p>
+            </div>
+        </section>
+
+
+            `;
+}
+
+repositoriosGitHub();
